0b040d986472223f87dcda37ab066d1fe05cd256
Tomasz Jagiełka <tomasz.m.jagielka@gmail.com>
2025-01-18 00:56:45 +0100

    Fix the wrong path and add a port

--- deploy.sh
+++ deploy.sh
@@ -8,13 +8,14 @@ FRONTEND_DIR="client"
 BACKEND_DIR="server"
 FRONTEND_BUILD_DIR="/var/www/ticketroo"
 BACKEND_ENTRY="src/index.js"
+BACKEND_PORT=3004
 
 cd $FRONTEND_DIR
 npm install
 npm run build
 
 sudo mkdir -p $FRONTEND_BUILD_DIR
-sudo cp -r dist/* $FRONTEND_BUILD_DIR/
+sudo cp -r build/* $FRONTEND_BUILD_DIR/
 
 sudo chown -R www-data:www-data $FRONTEND_BUILD_DIR
 sudo chmod -R 755 $FRONTEND_BUILD_DIR
@@ -22,7 +23,7 @@ sudo chmod -R 755 $FRONTEND_BUILD_DIR
 cd ../$BACKEND_DIR
 npm install
 
-pm2 restart $BACKEND_DIR/$BACKEND_ENTRY --name ticketroo-backend || pm2 start $BACKEND_ENTRY --name ticketroo-backend
+pm2 restart $BACKEND_ENTRY --name ticketroo-backend --update-env || pm2 start $BACKEND_ENTRY --name ticketroo-backend --update-env --env PORT=$BACKEND_PORT
 
 if curl -s http://localhost:80 | grep -q "Ticketroo"; then
   echo "Frontend is live!"
@@ -30,7 +31,7 @@ else
   echo "Frontend deployment verification failed. Please check the Nginx configuration."
 fi
 
-if curl -s http://localhost:3001/status; then
+if curl -s http://localhost:$BACKEND_PORT/status; then
   echo "Backend is live!"
 else
   echo "Backend deployment verification failed. Please check the backend logs."
